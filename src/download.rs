be141c5c23c65c1b40ade7eb25f4ea4a9f02a4ab
sc <146133436+samcalthrop@users.noreply.github.com>
2026-03-08 22:50:22 +0000

    Release v0.1.1

--- src/download.rs
+++ src/download.rs
@@ -3,13 +3,45 @@ use std::process::Command;
 use crate::prompts::Config;
 
 pub fn download(config: Config) {
-    Command::new("ytd")
-        .arg("-d")
-        .arg(&config.url)
-        .arg(config.file_format)
-        .arg(config.download_playlist.to_string())
-        .arg(config.destination)
-        .arg(config.download_thumbnail.to_string())
-        .status()
-        .unwrap();
+    let mut args: Vec<String> = vec![];
+    // Download audio?
+    if config.file_format == "mp3" {
+        args.push("-x".to_string());
+        args.push("--audio-format".to_string());
+        args.push("mp3".to_string());
+    // Download video?
+    } else if config.file_format == "mp4" {
+        args.push("--merge-output-format".to_string());
+        args.push("mp4".to_string());
+        args.push("--remux-video".to_string());
+        args.push("mp4".to_string());
+        args.push("-S".to_string());
+        args.push("vcodec:h264,lang,quality,res,fps,hdr:12".to_string());
+    }
+    // Download playlist?
+    if config.download_playlist {
+        args.push("-o".to_string());
+        args.push("\"%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s\"".to_string());
+    }
+    // Download thumbnail?
+    if config.download_thumbnail {
+        args.push("--write-thumbnail".to_string());
+    }
+    // Destination path
+    args.push("-P".to_string());
+    args.push(config.destination);
+    // URL
+    args.push(config.url);
+
+    Command::new("yt-dlp").args(args).status().unwrap();
+
+    // Command::new("ytd")
+    //     .arg("-d")
+    //     .arg(&config.url)
+    //     .arg(config.file_format)
+    //     .arg(config.download_playlist.to_string())
+    //     .arg(config.destination)
+    //     .arg(config.download_thumbnail.to_string())
+    //     .status()
+    //     .unwrap();
 }
